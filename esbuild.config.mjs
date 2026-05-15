3a8e2d46a6dae1458f85b06c951b51cff558fec9
Valentín Muro <valentinmuro@gmail.com>
2026-05-15 16:50:44 +0200

    Update esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from 'builtin-modules'
+import { builtinModules } from 'node:module'
 import path from "path";
 import { fileURLToPath } from 'url';
 import { copyFileSync } from 'fs';
@@ -49,7 +49,7 @@ const buildOptions = {
 		'@lezer/common',
 		'@lezer/highlight',
 		'@lezer/lr',
-		...builtins],
+		...builtinModules],
 	format: 'cjs',
 	target: 'es2016',
 	logLevel: "info",
